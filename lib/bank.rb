b0b4afc4903aa7e0af3c099baadb474f0733dd41
Jacob <jacobchanrahan@gmail.com>
2022-01-05 13:01:40 +0000

    formatting

--- lib/bank.rb
+++ lib/bank.rb
@@ -13,16 +13,17 @@ end
 
 def deposit(num2)
   @balance += num2.to_i
-  return (@statement << [bank_date(date), (" || credit: #{num2.to_i}"), (" || balance: #{@balance}")])
+  return (@statement << [bank_date(date), ("  ||  #{num2.to_i}"), ("    ||        "), (" || #{@balance}")])
 end
 
 def withdraw(num1)
   fail "Insufficent funds" if  @balance - num1.to_i < 0
   @balance -= num1.to_i
-  return (@statement << [bank_date(date), (" || debit: #{num1.to_i}"), (" || balance: #{@balance}")])
+  return (@statement << [bank_date(date), ("  ||        "), ("||   #{num1.to_i}"), ("    ||  #{@balance}")])
 end
 
 def state
+  puts "     Date    ||  Credit ||  Debit   ||  Balance"
   @statement.each do |date|
     puts date.join(" ")
     end
